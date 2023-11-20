c7bf0418d5a21dd965044cfeb2b4786e8c85e492
Wes Preece <wes.r.preece@gmail.com>
2023-11-20 22:04:09 +0100

    Clean up pre-testing

--- src/main/kotlin/com/portfolio/commons/Utility.kt
+++
@@ -1,7 +0,0 @@
-package com.portfolio.commons
-
-fun convertTo8bits(binary: String): String {
-    val zeros = StringBuilder("")
-    for (i in 0 until 8 - binary.length) zeros.append("0")
-    return zeros.toString() + binary
-}
